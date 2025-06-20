055b0aa8fa5715cfc4ae35c7b34ea8389128ffb2
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 02:25:49 +0000

    [automatic commit] 2025-06-20 02:25:49 UTC 193.115.107.98

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,6 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+router.get('/result', function(req, res, next) {
+//Connect to the database
+req.pool.getConnection( function(err,connection) {
+if (err) {
+res.sendStatus(500);
+return;
+}
+var query = "SHOW TABLES";
+connection.query(query, function(err, rows, fields) {
+connection.release(); // release connection
+if (err) {
+res.sendStatus(500);
+return;
+}
+res.json(rows); //send response
+});
+});
+});
 
 module.exports = router;
