109b8594a363c7ec6fd13c1752fe620a65bb73cb
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 03:11:56 +0000

    [automatic commit] 2025-06-20 03:11:56 UTC 193.115.107.98

--- part1/app.js
+++ part1/app.js
@@ -21,9 +21,9 @@ let db;
   try {
     // Connect to MySQL without specifying a database
     const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
+      host: 'localhost'
+      // user: 'root',
+      // password: '' // Set your MySQL root password
     });
 
     // Create the database if it doesn't exist
