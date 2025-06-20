2b1833d0489d9b81c22d0f70603ca5c9781576f0
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 03:07:07 +0000

    [automatic commit] 2025-06-20 03:07:06 UTC 193.115.107.98

--- part1/app.js
+++ part1/app.js
@@ -8,6 +8,9 @@ var usersRouter = require('./routes/users');
 
 // use mysql in this app
 var mysql = require('mysql');
+
+
+
 // create a 'pool' (group) of connections to be used for connecting with our SQL server
 var dbConnectionPool = mysql.createPool({
   host: 'localhost',
