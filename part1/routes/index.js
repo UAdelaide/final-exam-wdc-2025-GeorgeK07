81329d9aeb2cdbc2ef3d69f74f2176af48aca515
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 02:26:33 +0000

    [automatic commit] 2025-06-20 02:26:32 UTC 193.115.107.98

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -10,11 +10,11 @@ router.get('/result', function(req, res, next) {
   //Connect to the database
   req.pool.getConnection( function(err,connection) {
     if (err) {
-    res.sendStatus(500);
-    return;
-  }
-var query = "SHOW TABLES";
-connection.query(query, function(err, rows, fields) {
+      res.sendStatus(500);
+      return;
+    }
+    var query = "SHOW TABLES";
+    connection.query(query, function(err, rows, fields) {
 connection.release(); // release connection
 if (err) {
 res.sendStatus(500);
