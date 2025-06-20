2c67daf6b872204066ca7d2ac9d6e4758a88feb3
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 02:33:36 +0000

    [automatic commit] 2025-06-20 02:33:35 UTC 193.115.107.98

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -14,7 +14,7 @@ router.get('/api/dogs', function(req, res, next) {
       res.sendStatus(500);
       return;
     }
-    var query = "SELECT ";
+    var query = "SELECT name,size,username FROM Users INNER JOIN Dogs ON Users.user_id = Dogs.owner_id";
     connection.query(query, function(err, rows, fields) {
       connection.release(); // release connection
       if (err) {
