a5401c268fcdddca7d42c9783cacf67d53f3fd88
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 15:26:36 +0000

    [automatic commit] 2025-06-20 15:26:36 UTC 193.115.107.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -61,6 +61,7 @@ router.post('/users/login', async (req, res) => {
     // Get the user
     const [rows] = await db.query('SELECT username, password_hash FROM Users WHERE username = ?', [req.body.user]);
     console.log(rows);
+    console.log(rows);
     console.log(req.body.pass);
     if (rows.password_hash === req.body.pass) {
       res.json(rows);
