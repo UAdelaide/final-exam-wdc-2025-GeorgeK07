789600812e8ea87c173afa7d7e691f1d9cf39749
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 03:40:14 +0000

    [automatic commit] 2025-06-20 03:40:14 UTC 193.115.107.98

--- part1/app.js
+++ part1/app.js
@@ -30,7 +30,7 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-        ((SELECT request_id FROM WalkRequest WHERE name = "Max")),
+        (1, ),
         (),
         ()
       `);
