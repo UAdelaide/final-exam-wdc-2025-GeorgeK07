24aeb8d84992415b9e07f8b8b4c9e9281e6907a3
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 03:11:13 +0000

    [automatic commit] 2025-06-20 03:11:12 UTC 193.115.107.98

--- part1/app.js
+++ part1/app.js
@@ -7,27 +7,7 @@ var mysql = require('mysql2/promise');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-// use mysql in this app
-var mysql = require('mysql');
-// create a 'pool' (group) of connections to be used for connecting with our SQL server
-var dbConnectionPool = mysql.createPool({
-  host: 'localhost',
-  database: 'DogWalkService'
-});
-
 var app = express();
-
-//  Middleware for accessing database: We need access to the database to be
-// available *before* we process routes in index.js, so this code needs to be
-// *before* the app.use('/', routes);
-//  Express will run this function on every request and then continue with the
-// next module, index.js.
-//  So for all requests that we handle in index.js, we’ll be able to access the
-// pool of connections using req.pool
-app.use(function(req, res, next) {
-  req.pool = dbConnectionPool;
-  next();
-});
 
 app.use(logger('dev'));
 app.use(express.json());
