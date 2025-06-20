b84bf300ee5da45706e6d1186aa17b72670c959a
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-20 04:03:58 +0000

    [automatic commit] 2025-06-20 04:03:58 UTC 193.115.107.98

--- part1/app.js
+++ part1/app.js
@@ -32,7 +32,7 @@ let db;
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
         (3, 2, 5, 5, 'My dog is very happy.'),
         (1, 2, 3, 3, 'My dog is tired after the walk.'),
-        (3, 2, 5, 5, 'My dog is very happy.'),
+        (2, 4, 2, 5, 'My dog is very happy.'),
         (3, 2, 5, 5, 'My dog is very happy.'),
         (3, 2, 5, 5, 'My dog is very happy.')
       `);
