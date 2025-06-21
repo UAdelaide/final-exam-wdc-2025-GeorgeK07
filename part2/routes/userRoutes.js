637d65bbe86d100cb081482081c2304d5ba3cf23
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 03:56:15 +0000

    [automatic commit] 2025-06-21 03:56:15 UTC 193.115.107.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -81,6 +81,7 @@ router.post('/users/logout', async (req, res) => {
   try {
     // Destroy session
     req.session.destroy();
+    // Delete cookie
     res.clearCookie("SessionCookie", { path: "/" });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
