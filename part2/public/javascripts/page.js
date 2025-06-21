b19fde0614f8c6fc48e8bfd9d4929f0371f7e1f6
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 07:12:47 +0000

    [automatic commit] 2025-06-21 07:12:46 UTC ;; communications error to 208.67.222.222#53: timed out 193.115.107.98

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -265,7 +265,6 @@ async function getCurrentUser() {
         // Run code if ready state is 4 and status is 200
         if (this.readyState === 4 && this.status === 200) {
             // Return id of user
-            console.log(JSON.parse(xmlhttp.responseText));
             return JSON.parse(xmlhttp.responseText);
         }
     };
