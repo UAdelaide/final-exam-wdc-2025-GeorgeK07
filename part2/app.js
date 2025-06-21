bdbc1f03d37b5a22477e9356f935962285dee65e
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 03:04:24 +0000

    [automatic commit] 2025-06-21 03:04:23 UTC 193.115.107.98

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// 
+app.use(session({ // //
+secret: ‘a string of your choice’, // //
+resave: false, // THIS CODE //
+saveUninitialized: true, // //
+cookie: { secure: false } // //
+}));
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
