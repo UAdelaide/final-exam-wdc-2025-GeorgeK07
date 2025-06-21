0866864cd86b53f1cf1400c45eced172eed4f3e4
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 04:37:23 +0000

    [automatic commit] 2025-06-21 04:37:23 UTC 193.115.107.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,7 +35,7 @@ router.get('/me', async (req, res) => {
   }
   // Get the user's dogs from database
   const [rows] = await db.query('SELECT name FROM Dogs WHERE owner_id = ?', [req.body.user]);
-  res.json(rows[0].name);
+  res.json(rows);
 });
 
 // POST login (dummy version)
