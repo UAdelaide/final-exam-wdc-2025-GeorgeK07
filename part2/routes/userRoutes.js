d266d4080291b003640d2cdbe958b65591640a85
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 03:25:46 +0000

    [automatic commit] 2025-06-21 03:25:45 UTC ;; communications error to 208.67.222.222#53: timed out 193.115.107.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post('/users/login', async (req, res) => {
       req.session.password_hash = rows[0].password_hash;
       req.session.role = rows[0].role;
       // console.log(req.session);
-      res.json(rows[0].username);
+      res.json(rows[0].role);
     } else {
       throw new Error('Error');
     }
