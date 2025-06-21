626a184b46137766c0afa82dda519ec1fbcd58d6
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 06:53:27 +0000

    [automatic commit] 2025-06-21 06:53:26 UTC 193.115.107.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -106,7 +106,7 @@ router.post('/users/logout', async (req, res) => {
 router.get('/api/dogs', async(req, res, next) => {
   // Try catch for getting dog info
   try {
-    const [rows] = await db.query("SELECT dog_id,name,size, FROM Dogs");
+    const [rows] = await db.query("SELECT dog_id,owner_id,name,size FROM Dogs");
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch dogs" });
