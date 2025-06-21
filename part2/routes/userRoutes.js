e0bb28c0e3680b3b8fccddaca685606a458b8dc9
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 04:49:40 +0000

    [automatic commit] 2025-06-21 04:49:40 UTC 193.115.107.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,12 +72,6 @@ router.post('/users/login', async (req, res) => {
       req.session.email = rows[0].email;
       req.session.password_hash = rows[0].password_hash;
       req.session.role = rows[0].role;
-      console.log(req.session.user_id);
-      console.log(req.session.username);
-      console.log(req.session.email);
-      console.log(req.session.password_hash);
-      console.log(req.session.role);
-      console.log(rows);
       // Send role back to check which page to go to in login()
       res.json(rows[0].role);
     } else { // If not throw error
