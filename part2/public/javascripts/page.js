fa7e20d3eca544b952267b38191da5882cd83f56
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 06:54:11 +0000

    [automatic commit] 2025-06-21 06:54:10 UTC 193.115.107.98

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -301,7 +301,7 @@ function addDogsTable(){
                 let dog_owner_id = dog_row.insertCell();
                 dog_owner_id.innerText = dog_info_array[i].owner_id;
                 let dog_photo = dog_row.insertCell();
-                dog_photo.innerHTML = "<a href='https://dog.ceo/api/breeds/image/random'></a>";
+                dog_photo.innerHTML = "<img href='https://dog.ceo/api/breeds/image/random'></a>";
             }
         }
     };
