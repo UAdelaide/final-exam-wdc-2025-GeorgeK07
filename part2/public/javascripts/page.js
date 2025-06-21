bb5daf9f426ee4c7eaf44a1b9fd4264d0d364c70
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 04:53:21 +0000

    [automatic commit] 2025-06-21 04:53:21 UTC 193.115.107.98

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -240,9 +240,10 @@ function loadMyDogs(){
             console.log(xmlhttp.responseText);
             let dogNames = JSON.parse(xmlhttp.responseText);
             let drop_down = document.getElementById("dog_names");
-            for (let i = 0; i < ) {
+            for (let i = 0; i < dogNames.length; i++) {
                 let new_dog_name = document.createElement("option");
                 new_dog_name.text = dogNames[i].name;
+                drop_down.add();
             }
         }
     };
