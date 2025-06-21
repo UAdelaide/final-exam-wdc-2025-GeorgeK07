aad5e02ffd0e6874c6cfaae27e4f909f0f665828
GregRoll <135295471+GeorgeK07@users.noreply.github.com>
2025-06-21 03:21:45 +0000

    [automatic commit] 2025-06-21 03:21:44 UTC 193.115.107.98

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -189,8 +189,9 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            let username = JSON.parse(this.responseText);
-            alert("Welcome " + username);
+            console.log(this.responseText);
+            //let username = JSON.parse(this.responseText);
+            //alert("Welcome " + username);
 
         } else if (this.readyState === 4 && this.status >= 400) {
             alert("Login failed");
